10ba826fb0f0a06e1fb7fb77b8059bcf667fa3da
Kamol774 <chust1222@gmail.com>
2024-07-02 11:35:47 +0500

    feat: integration GraphQlModule and build first GraphQl api

--- apps/sneakers-api/src/app.module.ts
+++ apps/sneakers-api/src/app.module.ts
@@ -2,10 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		GraphQLModule.forRoot({
+			driver: ApolloDriver,
+			playground: true,
+			uploads: false,
+			autoSchemaFile: true,
+			// formatError: (error: T) => {
+			// 	const graphQLFormattedError = {
+			// 		code: error?.extensions.code,
+			// 		message:
+			// 			error?.extensions?.exception?.response?.message || error?.extensions?.response?.message || error?.message,
+			// 	};
+			// 	console.log('GRAPHQL GLOBAL ERROR:', graphQLFormattedError);
+			// 	return graphQLFormattedError;
+			// },
+		}),
+	],
+
+	controllers: [AppController],
+	providers: [AppService, AppResolver],
 })
 export class AppModule {}
